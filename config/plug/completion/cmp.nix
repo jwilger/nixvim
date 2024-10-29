0d4177a50335a8fbfd407d9db80c5a454818db90
John Wilger <john@johnwilger.com>
2024-10-29 16:26:36 -0700

    Change keybindings for completions
    Enter no longer auto-selects the first item in the list (allowing one to
    actually type a literal line break without accidentally accepting a
    completion.)
    
    Next/Prev completion selection now mapped to C-n/C-p, respectively.

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -57,14 +57,14 @@
         };
 
         mapping = {
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
           "<C-e>" = "cmp.mapping.abort()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          "<CR>" = "cmp.mapping.confirm()";
+          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace })";
         };
       };
     };
