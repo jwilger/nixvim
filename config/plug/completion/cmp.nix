39357b370e10f0c3c1ae149330a3f8cb19bb9a3a
John Wilger <john@johnwilger.com>
2024-10-29 13:59:48 -0700

    Don't use TAB in completions
    Because sometimes you want to type a damned tab

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -57,7 +57,6 @@
         };
 
         mapping = {
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
           "<C-j>" = "cmp.mapping.select_next_item()";
           "<C-k>" = "cmp.mapping.select_prev_item()";
           "<C-e>" = "cmp.mapping.abort()";
